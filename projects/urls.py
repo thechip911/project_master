47f8d030e6fa82002c6200eb5664f231171fa552
Chip <alok.sharma@trootech.com>
2021-07-26 16:47:36 +0530

    Minor Fix

--- projects/urls.py
+++ projects/urls.py
@@ -16,10 +16,12 @@ urlpatterns = [
     path('<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
     path('project_dashboard/', ProjectDashBoard.as_view(), name='project_dashboard'),
     path('<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
+
     path('task_create/', TaskCreateView.as_view(), name='task_create'),
     path('task/<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
     path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
     path('task_dashboard/', TaskDashBoard.as_view(), name='task_dashboard'),
+
     path('time_sheet_create/', TimeSheetCreateView.as_view(), name='time_sheet_create'),
     path('time_sheet/<int:pk>/update/', TimeSheetUpdateView.as_view(), name='time_sheet_update'),
     path('time_sheet_dashboard/', TimeSheetDashBoard.as_view(), name='time_sheet_dashboard'),
